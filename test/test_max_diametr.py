8df5b7bc32597796832d7531733121cbd5476cb5
Stas Zhurakovskyi <stas290606@gmail.com>
2024-03-18 12:58:20 +0200

    Added 1 test and fix pyth to file

--- test/test_max_diametr.py
+++ test/test_max_diametr.py
@@ -1,10 +1,17 @@
+import os
+import sys
 import unittest
-from max_diametr import BinaryTree, binary_tree_diameter
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+
+from src.max_diametr import BinaryTree, binary_tree_diameter
 
 
-class TestMinimalPosition(unittest.TestCase):
+class TestMaxDiameter(unittest.TestCase):
 
-    def test_data_lecture(self):
+    def test_binary_tree_diameter_data_lecture(self):
         root = BinaryTree(1)
         root.left = BinaryTree(3)
         root.right = BinaryTree(2)
@@ -16,7 +23,7 @@ class TestMinimalPosition(unittest.TestCase):
         root.left.right.right.right = BinaryTree(6)
         self.assertEquals(binary_tree_diameter(root), 6)
 
-    def test_data_student(self):
+    def test_binary_tree_diameter_data_student(self):
         root = BinaryTree(1)
         root.left = BinaryTree(3)
         root.right = BinaryTree(4)
@@ -29,6 +36,24 @@ class TestMinimalPosition(unittest.TestCase):
         root.left.left.right.right.right = BinaryTree(1)
         root.left.left.right.right.right.left = BinaryTree(4)
         self.assertEquals(binary_tree_diameter(root), 7)
+
+    def test_binary_tree_diameter_the_hardest_data(self):
+        root = BinaryTree(100)
+        root.left = BinaryTree(50)
+        root.right = BinaryTree(120)
+        root.left.left = BinaryTree(10)
+        root.right.right = BinaryTree(125)
+        root.right.left = BinaryTree(105)
+        root.left.right = BinaryTree(71)
+        root.left.left.left = BinaryTree(8)
+        root.left.left.right = BinaryTree(12)
+        root.left.left.left.left = BinaryTree(6)
+        root.left.left.left.left.left = BinaryTree(5)
+        root.left.left.right.right = BinaryTree(15)
+        root.left.left.right.right.right = BinaryTree(18)
+        root.left.left.right.right.right.left = BinaryTree(19)
+        root.left.left.right.right.right.left.left = BinaryTree(20)
+        self.assertEquals(binary_tree_diameter(root), 9)
 
 
 if __name__ == "__main__":
