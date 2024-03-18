7cdda288b76153c80bdba72e4a34278489ff4810
Stas Zhurakovskyi <stas290606@gmail.com>
2024-03-18 21:59:14 +0200

    Сhanging the option in the code
    At first I did not perform my option, so now I have to add my option

--- src/heap_based_priority_queue.py
+++ src/heap_based_priority_queue.py
@@ -1,76 +1,66 @@
+# Fourth lab 2 level 1 option
+
 class Node:
-    def __init__(self, value, priority, left=None, right=None):
+    def __init__(self, value, priority):
         self.value = value
         self.priority = priority
-        self.left = left
-        self.right = right
 
 
 class BinaryHeap:
     def __init__(self):
-        self.root = None
+        self.priority_queue = []
+
+    def swap_two_elements(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
 
     def add_element(self, value, priority):
-        if self.root is None:
-            self.root = Node(value, priority)
-        else:
-            self.add_recursively(value, priority, self.root)
+        new_node = Node(value, priority)
+        self.priority_queue.append((new_node.value, new_node.priority))
+        if len(self.priority_queue) > 1:
+            for i in range((len(self.priority_queue)-2) // 2, -1, -1):
+                self.build_heapify(self.priority_queue, i, len(self.priority_queue))
 
-    def add_recursively(self, value, priority, node):
-        if node.priority <= priority:
-            if node.right is None:
-                node.right = Node(value, priority)
-            else:
-                self.add_recursively(value, priority, node.right)
-        else:
-            if node.left is None:
-                node.left = Node(value, priority)
-            else:
-                self.add_recursively(value, priority, node.left)
+    def build_heapify(self, arr, pos_node, heap_size):
+        left_child, right_child = pos_node * 2 + 1, pos_node * 2 + 2
+        if max(left_child, right_child) < heap_size:
+            if arr[left_child][1] > arr[pos_node][1]:
+                self.swap_two_elements(arr, left_child, pos_node)
+                self.build_heapify(arr, left_child, heap_size)
+            if arr[right_child][1] > arr[pos_node][1]:
+                self.swap_two_elements(arr, right_child, pos_node)
+                self.build_heapify(arr, right_child, heap_size)
+        elif left_child < heap_size:
+            if arr[left_child][1] > arr[pos_node][1]:
+                self.swap_two_elements(arr, left_child, pos_node)
+                self.build_heapify(arr, left_child, heap_size)
+        elif right_child < heap_size:
+            if arr[right_child][1] > arr[pos_node][1]:
+                self.swap_two_elements(arr, right_child, pos_node)
+                self.build_heapify(arr, right_child, heap_size)
 
-    def find_the_biggest(self):
-        cur_node = self.root
-        if cur_node.right is not None:
-            while cur_node.right.right is not None:
-                cur_node = cur_node.right
-            return cur_node, cur_node.right
-        else:
-            return cur_node, None
+    def pop_element(self):
+        top_element = self.priority_queue[0]
+        self.priority_queue[0] = self.priority_queue[-1]
+        self.priority_queue.pop()
+        for i in range(0, (len(self.priority_queue) - 2) // 2):
+            self.build_heapify(self.priority_queue, i, len(self.priority_queue))
+        print(top_element)
+        return top_element
 
-    def delete(self):
-        node_to_delete = self.find_the_biggest()
-        if node_to_delete[1] is not None:
-            node_to_delete[0].right = node_to_delete[1].left
-            return node_to_delete[1].value, node_to_delete[1].priority
-        else:
-            self.root = node_to_delete[0].left
-            return node_to_delete[0].value, node_to_delete[0].priority
-
-    def show_heap(self, node):
-        show_que = []
-
-        def create_que(node):
-            nonlocal show_que
-            if node is None:
-                return
-            show_que.append((node.value, node.priority))
-            create_que(node.left)
-            create_que(node.right)
-        create_que(node)
-        show_que.sort(key=lambda x: x[1], reverse=True)
-        print(show_que)
-        return show_que
+    def show_queue(self):
+        print(self.priority_queue)
+        return self.priority_queue
 
 
 if __name__ == "__main__":
     heap = BinaryHeap()
     heap.add_element(5, 5)
-    heap.add_element(15, 4)
-    heap.add_element(7, 3)
-    heap.add_element(2, 6)
-    heap.add_element(9, 10)
-    heap.add_element(8, 9)
-
-    heap.show_heap(heap.root)
-    print(heap.delete())
-    heap.show_heap(heap.root)
+    heap.add_element(9, 16)
+    heap.add_element(10, 8)
+    heap.add_element(4, 14)
+    heap.add_element(4, 20)
+    heap.add_element(4, 1)
+    heap.add_element(4, 26)
+    heap.show_queue()
+    heap.pop_element()
+    heap.show_queue()
--- test/test_heap_based_priority.py
+++ test/test_heap_based_priority.py
@@ -1,12 +1,5 @@
-import os
-import sys
 import unittest
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-sys.path.append(parent_dir)
-
-from src.heap_based_priority_queue import BinaryHeap
+from Algo_and_data_structures.src.heap_based_priority_queue import BinaryHeap
 
 
 class TestHeapBasedPriority(unittest.TestCase):
@@ -14,25 +7,25 @@ class TestHeapBasedPriority(unittest.TestCase):
     def test_add_element(self):
         heap = BinaryHeap()
         heap.add_element(5, 5)
-        self.assertEquals(heap.show_heap(heap.root), [(5, 5)])
+        self.assertEquals(heap.show_queue(), [(5, 5)])
 
-    def test_delete_from_root(self):
+    def test_pop_element(self):
         heap = BinaryHeap()
         heap.add_element(5, 5)
-        heap.add_element(3, 3)
-        heap.add_element(1, 1)
-        self.assertEquals(heap.delete(), (5, 5))
+        heap.add_element(9, 16)
+        heap.add_element(10, 8)
+        heap.add_element(4, 14)
+        heap.add_element(4, 20)
+        heap.add_element(4, 1)
+        heap.add_element(4, 26)
+        self.assertEquals(heap.pop_element(), (4, 26))
 
-    def test_delete_from_right(self):
+    def test_add_element_if_build_heap(self):
         heap = BinaryHeap()
-        heap.add_element(5, 5)
-        heap.add_element(15, 4)
-        heap.add_element(7, 3)
-        heap.add_element(2, 6)
-        heap.add_element(9, 10)
-        heap.add_element(1, 12)
-        heap.add_element(8, 9)
-        self.assertEquals(heap.delete(), (1, 12))
+        heap.add_element(5, 1)
+        heap.add_element(15, 2)
+        heap.add_element(7, 5)
+        self.assertEquals(heap.show_queue(), [(7, 5), (5, 1), (15, 2)])
 
 
 if __name__ == "__main__":
